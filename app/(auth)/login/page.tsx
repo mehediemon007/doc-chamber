9b464f8154a7431b097e08c798c4537d55256102
mehediemon007 <mehediemon8=7@gmail.com>
2026-01-28 00:06:35 +0600

    complete-login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,30 +3,61 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
+import { signIn, SignInResponse } from 'next-auth/react';
+import { toast } from 'sonner';
+
 // *** Import Icons
-import { Loader2 } from 'lucide-react';
+import { Loader2, CircleAlert } from 'lucide-react';
+
+type LoginFormInputs = {
+    phone: string,
+    password: string,
+}
 
 export default function Login() {
-
-    const [formData, setFormData] = useState({
-        phone: '',
-        password: ''
-    });
-
+    const router = useRouter();
     const [loading, setLoading] = useState(false);
+    const [serverError, setServerError] = useState('');
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData((prev) => ({...prev, [e.target.id] : e.target.value}))
-    }
+    const { register, handleSubmit, reset, formState: { errors }} = useForm<LoginFormInputs>();
 
-    const handleLogin = async (e : React.FormEvent) => {
-        e.preventDefault();
-        setLoading(true)
+    const onSubmit = async (data : LoginFormInputs) => {
+        setServerError('');
+        setLoading(true);
+
+        try {
+
+            const result  = await signIn("login", {
+                identifier: data.phone,
+                password: data.password,
+                redirect: false,
+            }) as SignInResponse;
+
+            if (result?.error) {
+                setServerError("Invalid phone number or password");
+            } else {
+                reset();
+                toast.success('Login Successful!!');
+                router.push('/dashboard');
+            }
+
+        } catch (error: unknown){
+
+            console.error(error);
+            setServerError(
+                error instanceof Error ? error.message : 'Something went wrong. Please try again.'
+            );
+
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -36,20 +67,35 @@ export default function Login() {
                     <div className="grid grid-cols-1 md:grid-cols-2 items-center w-full sm:max-w-5xl shadow-[0_4px_12px_rgba(0,0,0,0.1)] rounded-xl sm:rounded-2xl">
                         <div className='col-span-1 py-6 px-4 sm:py-8 sm:px-6 md:py-20 md:px-12'>
                             <h2 className='text-xl sm:text-2xl xl:text-4xl text-primary text-center md:text-left mb-4 sm:mb-6'>Login To Your Account!</h2>
-                            <form onSubmit={handleLogin}>
+                            {serverError && <p className='inline-flex items-center gap-1 text-sm text-red-500 mb-2'><CircleAlert size={16}/> {serverError}</p>}
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div className="space-y-2 mb-3 sm:mb-4">
                                     <Label htmlFor="phone" className='required'>Phone Number</Label>
-                                    <Input id="phone" type="tel" required value={formData.phone} placeholder='01XXX-XXXXXX' onChange={handleChange} />
+                                    <Input id="phone" type="tel" placeholder='01XXX-XXXXXX' {...register('phone', {required: 'Phone Number is required'})} className={errors.phone ? 'border-red-500 focus-visible:border-red-500' : ''} onFocus={() => setServerError('')}/>
+                                    {
+                                        errors.phone && (
+                                            <p className='text-sm text-red-500 flex items-center gap-1'>
+                                                <CircleAlert size={16}/> {errors.phone.message}
+                                            </p>
+                                        )
+                                    }
                                 </div>
 
                                 <div className="relative space-y-2 mb-3 sm:mb-4">
                                     <Label htmlFor="password" className='required'>Password</Label>
-                                    <Input id="password" type="password" required value={formData.password} placeholder='password1234' autoComplete='off' onChange={handleChange} />
+                                    <Input id="password" type="password" placeholder='password1234' autoComplete='off' {...register('password', { required: 'Password is required'})} className={errors.password ? 'border-red-500 focus-visible:border-red-500' : ''} onFocus={() => setServerError('')}/>
+                                    {
+                                        errors.password && (
+                                            <p className='text-sm text-red-500 flex items-center gap-1'>
+                                                <CircleAlert size={16}/> {errors.password.message}
+                                            </p>
+                                        )
+                                    }
                                 </div>
 
                                 <Button type="submit" className="w-full mt-1 sm:mt-2" disabled={loading}>
                                     {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                                    {loading ? "Creating Account..." : "Login"}
+                                    {loading ? "Logging in..." : "Login"}
                                 </Button>
 
                             </form>
